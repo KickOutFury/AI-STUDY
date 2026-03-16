340d64452666eb339c3cf0bbb0c297b0cec88688
KickOutFury <hyeokjiny01@gmail.com>
2026-03-16 18:43:09 +0900

    solve 11720

---
+++ BAEKJOON/Step5/11720.py
@@ -0,0 +1,14 @@
+n = int(input())
+number = input()
+total = 0
+
+for x in number:
+    total += int(x)
+    
+print(total)
+
+# 좀 더 간단하게 축소
+# n = int(input())
+# numbers = input()
+
+# print(sum(map(int, numbers)))
